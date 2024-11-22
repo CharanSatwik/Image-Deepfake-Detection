a9c772412bdb3713e0068ada78709e6210e33b29
Charan Satwik <112375922+CharanSatwik@users.noreply.github.com>
2024-11-22 22:55:35 +0530

    Update Image_Deepfake_Detection.py

--- Image_Deepfake_Detection.py
+++ Image_Deepfake_Detection.py
@@ -4,36 +4,45 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the saved model
+# Load the pre-trained deepfake detection model
 model = tf.keras.models.load_model("deepfake-detector-model.keras")
 
-# Function to preprocess the image
 def preprocess_image(image):
     img = image.resize((128, 128))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
-# Streamlit app
-st.title("Image Deepfake Detection")
+def predict_deepfake(image_path):
+    try:
+        image = Image.open(image_path)
+        preprocessed_image = preprocess_image(image)
+        prediction = model.predict(preprocessed_image)
+        predicted_label = np.round(prediction)[0][0]
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+        if predicted_label == 1:
+            return "Fake"
+        else:
+            return "Real"
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        return "Error"
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+def main():
+    st.title("Deepfake Detection App")
 
-    preprocessed_image = preprocess_image(image)
-    prediction = model.predict(preprocessed_image)
-    predicted_labels = np.round(prediction)
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    st.write(f"Predicted Label: {'Fake' if prediction[0][0] > 0.61 else 'Real'}")
-    
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
 
+        prediction_result = predict_deepfake(uploaded_file)
 
-    # Display the image and prediction using matplotlib within Streamlit
-    fig, ax = plt.subplots()
-    ax.imshow(image)
-    ax.axis('off')
-    ax.set_title(f"Predicted Label: {'Fake' if prediction[0][0] > 0.61 else 'Real'}")
-    st.pyplot(fig)
+        if prediction_result == "Error":
+            st.error("An error occurred while processing the image.")
+        else:
+            st.write(f"Predicted Label: {prediction_result}")
+
+if __name__ == "__main__":
+    main()
